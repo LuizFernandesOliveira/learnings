adb3f0d73fcd157cb5ab102eeac2dbde49547712
LuizFernandesOliveira <luizfernandesoliveiraoficial@gmail.com>
2022-02-14 21:17:09 -0300

    Ternary Operator

---
+++ java/version11/flow_control/TernaryOperator.java
@@ -0,0 +1,10 @@
+public class TernaryOperator {
+  public static void main(String[] args) {
+    int value = 5;
+    int number;
+
+    number = value == 10 ? value : 10;
+
+    System.out.println(number);
+  }
+}
