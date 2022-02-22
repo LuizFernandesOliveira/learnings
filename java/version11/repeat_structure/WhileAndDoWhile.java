d9c3323dd42c71afb961b009897adba31788c7b7
LuizFernandesOliveira <luiz.fernandes@academico.ifrn.edu.br>
2022-02-22 20:44:28 -0300

    feat: while and do while

--- java/version11/repeat_structure/WhileAndDoWhile.java
+++ java/version11/repeat_structure/WhileAndDoWhile.java
@@ -3,14 +3,30 @@ import java.util.Scanner;
 public class WhileAndDoWhile {
   public static void main(String[] args) {
 
-    Integer age;
+    Integer age = 1;
     String name;
     
     var input = new Scanner(System.in);
-    
-    System.out.println("Digite seu nome:");
 
-    age = input.nextInt();
+    while (age > 0) {
+      System.out.println("Digite seu nome:");
+      name = input.nextLine();
+
+      System.out.println("Digite sua idade:");
+      age = Integer.parseInt(input.nextLine());
+
+      System.out.println(name + " tem " + age + " anos.");
+    }
+
+    do {
+      System.out.println("Digite seu nome:");
+      name = input.nextLine();
+
+      System.out.println("Digite sua idade:");
+      age = Integer.parseInt(input.nextLine());
+
+      System.out.println(name + " tem " + age + " anos.");
+    } while (age > 1);
 
     input.close();
   }
