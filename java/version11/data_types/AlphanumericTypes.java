7578781dbada7222d215524b4791bb96cec04668
LuizFernandesOliveira <luiz.fernandes@academico.ifrn.edu.br>
2022-02-22 21:26:43 -0300

    feat: alphanumeric types

---
+++ java/version11/data_types/AlphanumericTypes.java
@@ -0,0 +1,10 @@
+public class AlphanumericTypes {
+  public static void main(String[] args) {
+    // Primitive types
+    char alphanumericChar;
+
+    // Wrapper types
+    Character alphanumericCharWrapper;
+    String alphanumericString;
+  }
+}
