6773701fb31139b0046884bfca2db301361b39a0
LuizFernandesOliveira <luizfernandesoliveiraoficial@gmail.com>
2022-03-01 10:32:03 -0300

    Object Driven

---
+++ java/version11/object_driven/Classe.java
@@ -0,0 +1,12 @@
+public class Classe {
+  String attribute;
+
+  // constructor
+  public Classe(String attribute) {
+    this.attribute = attribute;
+  }
+
+  void method() {
+    String variable;
+  }
+}
