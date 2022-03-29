6960005f1fcea86992b6702723ef08051f8e499d
LuizFernandesOliveira <luizfernandesoliveiraoficial@gmail.com>
2022-03-28 21:33:17 -0300

    Threads

--- java/version11/thread/src/GenerateReport.java
+++ java/version11/thread/src/GenerateReport.java
@@ -1,10 +1,14 @@
 public class GenerateReport implements Runnable {
   public static int NUMBER = 1;
+  public static String CURR = "REPORT";
   public synchronized void execute() {
-    while (NUMBER < 100) {
+    while (NUMBER < 100 && GenerateReport.CURR.equals("REPORT")) {
       System.out.println(NUMBER + " - Generating report await...");
       NUMBER++;
+      CURR = "PROGRESS";
     }
+
+    CURR = "REPORT";
   }
 
   @Override
--- java/version11/thread/src/ProgressBar.java
+++ java/version11/thread/src/ProgressBar.java
@@ -1,10 +1,13 @@
 public class ProgressBar implements Runnable {
 
   public synchronized void execute() {
-    while (GenerateReport.NUMBER < 100) {
+    while (GenerateReport.NUMBER < 100 && GenerateReport.CURR.equals("PROGRESS")) {
       System.out.println(GenerateReport.NUMBER + " - In progress await...");
       GenerateReport.NUMBER++;
+      GenerateReport.CURR = "REPORT";
     }
+
+    GenerateReport.CURR = "PROGRESS";
   }
 
   @Override
